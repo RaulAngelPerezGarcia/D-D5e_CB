305cbc96570109fb054f12c0b41ab88934f03702
Raul Perez <zerepangel@gmail.com>
2016-10-25 10:32:48 +0200

    Added a template from bootstrap, modified index.html and main.css

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var fs 			= require('fs');
 var port 		= process.env.PORT || 8080;
 var router 		= express.Router(); 
 
-
-
 app.use(express.static(path.join(__dirname + '/node_modules')));
 
 app.get('/', function(req, res){
