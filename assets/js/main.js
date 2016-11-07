a919819436b4b5bb8f902a159a394c4e8a9a7939
Raul Perez <zerepangel@gmail.com>
2016-11-07 12:30:10 +0100

    Added pictures to the description of all races

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,7 @@ function incBase(idTag){
 			remaining();
 	}
 
-}
+};
 
 function decBase(idTag){
 	var base = +document.getElementById('disp'+idTag).value;
@@ -31,14 +31,14 @@ function decBase(idTag){
 		calculateAll(idTag)
 		remaining();
 	}
-}
+};
 
 // This function handles the remaining points 
 function remaining(){
 	var remp = +27 - +totalCost()
 	document.getElementById('remp').value = +remp;
 	return remp;
-}
+};
 
 function totalCost(){
 	var totCost = +document.getElementById('costStr').value 
@@ -48,7 +48,7 @@ function totalCost(){
 		+ +document.getElementById('costWis').value 
 		+ +document.getElementById('costCha').value;
 	return +totCost;
-}
+};
 
 
 // Sets the racial bonuses for each id
@@ -162,8 +162,9 @@ function selectedRace(id, textIn){
 	}
 	console.log(textIn);
 	dropdown(textIn, 'raceButton');
+	showRaceImage(id);
 	update();
-}
+};
 
 
 // Select the id function
@@ -173,14 +174,14 @@ function selectedClass(id, textIn){
 	var savingThrows
 	var proficiencies
 	dropdown(textIn, 'classButton');
-}
+};
 
 
 //This function sets the name of the dropdown to the selected token
 function dropdown(val, name) {
 	var y = document.getElementsByName(name);
 	var aNode = y[0].innerHTML = val + ' <span class="caret"></span>'; // Append 
-}
+};
 
 
 
@@ -198,7 +199,7 @@ function calculateAll(idTag){
 	calcMod(idTag);
 	document.getElementById('cost'+idTag).value = calcCost(document.getElementById('disp'+idTag).value);
 	remaining();
-}
+};
 
 
 // Here all the Scores values are calculated
@@ -206,7 +207,7 @@ function calcScore(idTag){
 	var base = document.getElementById('disp'+idTag).value;
 	var racial = document.getElementById('racial'+idTag).value;
 	document.getElementById('score' + idTag).value =  +base + +racial;
-}
+};
 
 
 // Here all the Modifiers values are calculated
@@ -219,7 +220,7 @@ function calcMod(idTag){
 	}else{
 		document.getElementById('modifier'+idTag).value = modifier;	
 	}
-}
+};
 
 
 function calcCost(number){
@@ -244,7 +245,7 @@ function calcCost(number){
 		cost = 27
 	}
 	return cost;
-}
+};
 
 
 function clearing(){
@@ -257,4 +258,62 @@ function clearing(){
 	document.getElementById('disp' + 'Wis').value = 8;
 	document.getElementById('disp' + 'Cha').value = 8;
 	update();
+};
+
+function showRaceImage(selRace){
+	var dv = document.getElementById('raceImage');
+	// remove all child nodes
+	while (dv.hasChildNodes()) { 
+		dv.removeChild(dv.lastChild); 
+	}
+	var img = document.createElement("IMG");
+
+	if(selRace == "human"){
+		img.setAttribute('src', 'http://cdn.obsidianportal.com/images/65157/keldar.jpg');
+		dv.appendChild(img);
+		console.log('Should be showing an image of a human')
+	} else if (selRace == "dwarfHill"){
+		img.setAttribute('src', 'http://vignette1.wikia.nocookie.net/shattered-lands/images/9/95/Ulfberht.jpg/revision/latest?cb=20130213153257');
+		dv.appendChild(img);
+	} else if (selRace == "dwarfMountain"){
+		img.setAttribute('src', 'http://vignette1.wikia.nocookie.net/shattered-lands/images/9/95/Ulfberht.jpg/revision/latest?cb=20130213153257');
+		dv.appendChild(img);
+	} else if (selRace == "elfHigh"){
+		img.setAttribute('src', 'http://varg.wdfiles.com/local--files/se-mage/19455High_Elf_Avatar_by_Graysun_D.jpg');
+		dv.appendChild(img);	
+	} else if (selRace == "elfWood"){
+		img.setAttribute('src', 'http://varg.wdfiles.com/local--files/se-mage/19455High_Elf_Avatar_by_Graysun_D.jpg');
+		dv.appendChild(img);	
+	} else if (selRace == "elfDrow"){
+		img.setAttribute('src', 'http://varg.wdfiles.com/local--files/se-mage/19455High_Elf_Avatar_by_Graysun_D.jpg');
+		dv.appendChild(img);	
+	} else if (selRace == "halflingLightfoot"){
+		img.setAttribute('src', 'https://db4sgowjqfwig.cloudfront.net/images/920420/Rosie_Cusswell.jpg');
+		dv.appendChild(img);	
+	} else if (selRace == "halflingStout"){
+		img.setAttribute('src', 'https://db4sgowjqfwig.cloudfront.net/images/920420/Rosie_Cusswell.jpg');
+		dv.appendChild(img);
+	} else if (selRace == "dragonborn"){	
+		img.setAttribute('src', 'https://s-media-cache-ak0.pinimg.com/564x/73/78/65/737865c6dbf5c9fdd4b7d982ae096b04.jpg');
+		dv.appendChild(img);
+	} else if (selRace == "gnomeForest"){
+		img.setAttribute('src', 'https://holiviantales.files.wordpress.com/2015/04/garl.png?w=470');
+		dv.appendChild(img);	
+	} else if (selRace == "gnomeRock"){
+		img.setAttribute('src', 'https://holiviantales.files.wordpress.com/2015/04/garl.png?w=470');
+		dv.appendChild(img);		
+	} else if (selRace == "halfElf"){
+		img.setAttribute('src', 'https://s-media-cache-ak0.pinimg.com/originals/02/01/f5/0201f5e43c30ed7fabc517c8254245c8.jpg');
+		dv.appendChild(img);	
+	} else if (selRace == "halfOrc"){
+		img.setAttribute('src', 'https://s-media-cache-ak0.pinimg.com/564x/98/2a/a5/982aa5c0b1a43606f472a9273c0c9bef.jpg');
+		dv.appendChild(img);	
+	} else if (selRace == "tiefling"){
+		img.setAttribute('src', 'https://s-media-cache-ak0.pinimg.com/236x/51/33/34/513334f69ce4f629a4024a07f6d49d97.jpg');
+		dv.appendChild(img);
+	}else {
+		img.setAttribute('src', 'https://thewhisperingway.files.wordpress.com/2013/07/386_ca_cleric2.jpg');
+		dv.appendChild(img);
+		console.log('Should be showing an image of a random guy')
+	}
 };
