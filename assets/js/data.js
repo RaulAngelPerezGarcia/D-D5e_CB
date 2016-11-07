428c1734e90d0df225fbb742b566d49397ff5a8f
Raul Perez <zerepangel@gmail.com>
2016-11-07 19:46:02 +0100

    added the data to data.js and reduced the code on main.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,140 +1,266 @@
 
 
-var race = {
+var dataRace = {
 	'human': {
+		'description' : 'Essentially, they are their 4e counterparts with vaguer backgrounds, dragonborn are still " \
+                        "pretty close to what they were. +2 Strength, +1 Charisma, and choose one Chromatic or " \
+                        "Metallic Dragon; they get a breath weapon shape, breath weapon damage, and damage resistance" \
+                        " based on what they chose (cold for Silver Dragonborn, acid for Black, etc).',
 		'racialStr' : 1,
 		'racialDex' : 1,
 		'racialCon' : 1,
 		'racialInt' : 1,
 		'racialWis' : 1,
 		'racialCha' : 1,
-		'imgScr' : 1
+		'imgScr' : 'http://cdn.obsidianportal.com/images/65157/keldar.jpg'
 	},	
 	'dwarfHill': {
+		'description' : '',
 		'racialStr' : 0,
 		'racialDex' : 0,
 		'racialCon' : 2,
 		'racialInt' : 0,
 		'racialWis' : 1,
 		'racialCha' : 0,
-		'imgScr' : 1
+		'imgScr' : 'http://vignette1.wikia.nocookie.net/shattered-lands/images/9/95/Ulfberht.jpg/revision/latest?cb=20130213153257'
 	},	
 	'dwarfMountain': {
+		'description' : '',
 		'racialStr' : 2,
 		'racialDex' : 0,
 		'racialCon' : 2,
 		'racialInt' : 0,
 		'racialWis' : 0,
 		'racialCha' : 0,
-		'imgScr' : 1
+		'imgScr' : 'http://vignette1.wikia.nocookie.net/shattered-lands/images/9/95/Ulfberht.jpg/revision/latest?cb=20130213153257'
 	},	
 	'elfHigh': {
+		'description' : '',
 		'racialStr' : 0,
 		'racialDex' : 2,
 		'racialCon' : 0,
 		'racialInt' : 1,
 		'racialWis' : 0,
 		'racialCha' : 0,
-		 'imgScr' : 1
+		 'imgScr' : 'http://varg.wdfiles.com/local--files/se-mage/19455High_Elf_Avatar_by_Graysun_D.jpg'
 	},	
 	'elfWood': {
+		'description' : '',
 		'racialStr' : 0,
 		'racialDex' : 2,
 		'racialCon' : 0,
 		'racialInt' : 0,
 		'racialWis' : 1,
 		'racialCha' : 0,
-		 'imgScr' : 1
+		 'imgScr' : 'http://varg.wdfiles.com/local--files/se-mage/19455High_Elf_Avatar_by_Graysun_D.jpg'
 	},	
 	'elfDrow': {
+		'description' : '',
 		'racialStr' : 0,
 		'racialDex' : 2,
 		'racialCon' : 0,
 		'racialInt' : 0,
 		'racialWis' : 0,
 		'racialCha' : 1,
-		 'imgScr' : 1
+		 'imgScr' : 'http://varg.wdfiles.com/local--files/se-mage/19455High_Elf_Avatar_by_Graysun_D.jpg'
 	},	
 	'halflingLightfoot': {
+		'description' : '',
 		'racialStr' : 0,
 		'racialDex' : 2,
 		'racialCon' : 0,
 		'racialInt' : 0,
 		'racialWis' : 0,
 		'racialCha' : 1,
-		'imgScr' : 0
+		'imgScr' : 'https://db4sgowjqfwig.cloudfront.net/images/920420/Rosie_Cusswell.jpg'
 		
 	},	
 	'halflingStout': {
+		'description' : '',
 		'racialStr' : 0,
 		'racialDex' : 2,
 		'racialCon' : 1,
 		'racialInt' : 0,
 		'racialWis' : 0,
 		'racialCha' : 0, 
-		'imgScr' : 0
+		'imgScr' : 'https://db4sgowjqfwig.cloudfront.net/images/920420/Rosie_Cusswell.jpg'
 	},	
 	'dragonborn': {
+		'description' : '',
 		'racialStr' : 2,
 		'racialDex' : 0,
 		'racialCon' : 0,
 		'racialInt' : 0,
 		'racialWis' : 0,
 		'racialCha' : 1, 
-		'imgScr' : 0
+		'imgScr' : 'https://s-media-cache-ak0.pinimg.com/564x/73/78/65/737865c6dbf5c9fdd4b7d982ae096b04.jpg'
 	},	
 	'gnomeForest': {
+		'description' : '',
 		'racialStr' : 0,
 		'racialDex' : 1,
 		'racialCon' : 0,
 		'racialInt' : 2,
 		'racialWis' : 0,
 		'racialCha' : 0, 
-		'imgScr' : 0
+		'imgScr' : 'https://holiviantales.files.wordpress.com/2015/04/garl.png?w=470'
 	},	
 	'gnomeRock': {
+		'description' : '',
 		'racialStr' : 0,
 		'racialDex' : 0,
 		'racialCon' : 1,
 		'racialInt' : 2,
 		'racialWis' : 0,
 		'racialCha' : 0, 
-		'imgScr' : 0
+		'imgScr' : 'https://holiviantales.files.wordpress.com/2015/04/garl.png?w=470'
 	},	
 	'halfElf': {
+		'description' : '',
 		'racialStr' : 2,
 		'racialDex' : 0,
 		'racialCon' : 0,
 		'racialInt' : 0,
 		'racialWis' : 0,
 		'racialCha' : 1, 
-		'imgScr' : 0
+		'imgScr' : 'https://s-media-cache-ak0.pinimg.com/originals/02/01/f5/0201f5e43c30ed7fabc517c8254245c8.jpg'
 	},	
 	'halfOrc': {
+		'description' : '',
 		'racialStr' : 2,
 		'racialDex' : 0,
 		'racialCon' : 1,
 		'racialInt' : 0,
 		'racialWis' : 0,
 		'racialCha' : 0, 
-		'imgScr' : 0
+		'imgScr' : 'https://s-media-cache-ak0.pinimg.com/564x/98/2a/a5/982aa5c0b1a43606f472a9273c0c9bef.jpg'
 	},	
 	'tiefling': {
+		'description' : '',
 		'racialStr' : 0,
 		'racialDex' : 0,
 		'racialCon' : 0,
 		'racialInt' : 1,
 		'racialWis' : 0,
 		'racialCha' : 2, 
-		'imgScr' : 0
+		'imgScr' : 'https://s-media-cache-ak0.pinimg.com/236x/51/33/34/513334f69ce4f629a4024a07f6d49d97.jpg'
 	},
-	'another': {
+	'none': {
+		'description' : '',
 		'racialStr' : 0,
 		'racialDex' : 0,
 		'racialCon' : 0,
 		'racialInt' : 0,
 		'racialWis' : 0,
 		'racialCha' : 0, 
-		'imgScr' : 0
+		'imgScr' : 'https://thewhisperingway.files.wordpress.com/2013/07/386_ca_cleric2.jpg'
+	}
+};
+
+var dataClass = {
+	'barbarian':{
+		'description' : 'A fierce warrior of primitive background who can enter a battle rage.',
+		'hitDie' : '',
+		'savingThrows' : '',
+		'armorProficiency' : '',
+		'weaponProficiency' : '',
+		'primaryAbility' : ''
+	},
+	'bard':{
+		'description' : 'An inspiring magician whose power echoes the music of creation.',
+		'hitDie' : '',
+		'savingThrows' : '',
+		'armorProficiency' : '',
+		'weaponProficiency' : '',
+		'primaryAbility' : ''
+	},
+	'cleric':{
+		'description' : 'A priestly champion who wields divine magic in service of a higher power.',
+		'hitDie' : '',
+		'savingThrows' : '',
+		'armorProficiency' : '',
+		'weaponProficiency' : '',
+		'primaryAbility' : ''
+	},
+	'druid':{
+		'description' : 'A priest of the Old Faith, wielding the powers of nature - moonlight and plant growth" \
+                             ", fire and lightning - and adopting animal forms.',
+		'hitDie' : '',
+		'savingThrows' : '',
+		'armorProficiency' : '',
+		'weaponProficiency' : '',
+		'primaryAbility' : ''
+	},
+	'fighter':{
+		'description' : 'A master of martial combat, skilled with a variety of weapons and armor.',
+		'hitDie' : '',
+		'savingThrows' : '',
+		'armorProficiency' : '',
+		'weaponProficiency' : '',
+		'primaryAbility' : ''
+	},
+	'monk':{
+		'description' : 'A master of martial arts, skilled with fighting hands and martial monk weapons.',
+		'hitDie' : '',
+		'savingThrows' : '',
+		'armorProficiency' : '',
+		'weaponProficiency' : '',
+		'primaryAbility' : ''
+	},
+	'paladin':{
+		'description' : 'A holy warrior bound to a sacred oath.',
+		'hitDie' : '',
+		'savingThrows' : '',
+		'armorProficiency' : '',
+		'weaponProficiency' : '',
+		'primaryAbility' : ''
+	},
+	'ranger':{
+		'description' : 'A master of ranged combat, one with nature.',
+		'hitDie' : '',
+		'savingThrows' : '',
+		'armorProficiency' : '',
+		'weaponProficiency' : '',
+		'primaryAbility' : ''
+	},
+	'rogue':{
+		'description' : 'A scoundrel who uses stealth and trickery to overcome obstacles and enemies.',
+		'hitDie' : '',
+		'savingThrows' : '',
+		'armorProficiency' : '',
+		'weaponProficiency' : '',
+		'primaryAbility' : ''
+	},
+	'sorcerer':{
+		'description' : 'A spell-caster who draws on inherent magic from a gift or bloodline.',
+		'hitDie' : '',
+		'savingThrows' : '',
+		'armorProficiency' : '',
+		'weaponProficiency' : '',
+		'primaryAbility' : ''
+	},
+	'warlock':{
+		'description' : 'A wielder of magic that is derived from a bargain with an extraplanar entity.',
+		'hitDie' : '',
+		'savingThrows' : '',
+		'armorProficiency' : '',
+		'weaponProficiency' : '',
+		'primaryAbility' : ''
+	},
+	'wizard':{
+		'description' : 'A scholarly magic-user capable of manipulating the structures of reality.',
+		'hitDie' : '',
+		'savingThrows' : '',
+		'armorProficiency' : '',
+		'weaponProficiency' : '',
+		'primaryAbility' : ''
+	},
+	'none2':{
+		'description' : '',
+		'hitDie' : '',
+		'savingThrows' : '',
+		'armorProficiency' : '',
+		'weaponProficiency' : '',
+		'primaryAbility' : ''
 	}
 };
--- assets/js/main.js
+++ assets/js/main.js
@@ -54,116 +54,18 @@ function totalCost(){
 // Sets the racial bonuses for each id
 function selectedRace(id, textIn){
 
-	if (id == "human"){
-		document.getElementById('racialStr').value = 1;
-		document.getElementById('racialDex').value = 1;
-		document.getElementById('racialCon').value = 1;
-		document.getElementById('racialInt').value = 1;
-		document.getElementById('racialWis').value = 1;
-		document.getElementById('racialCha').value = 1;		
-	} else if (id == "dwarfHill"){
-		document.getElementById('racialStr').value = 0;
-		document.getElementById('racialDex').value = 0;
-		document.getElementById('racialCon').value = 2;
-		document.getElementById('racialInt').value = 0;
-		document.getElementById('racialWis').value = 1;
-		document.getElementById('racialCha').value = 0;		
-	} else if (id == "dwarfMountain"){
-		document.getElementById('racialStr').value = 2;
-		document.getElementById('racialDex').value = 0;
-		document.getElementById('racialCon').value = 2;
-		document.getElementById('racialInt').value = 0;
-		document.getElementById('racialWis').value = 0;
-		document.getElementById('racialCha').value = 0;		
-	} else if (id == "elfHigh"){
-		document.getElementById('racialStr').value = 0;
-		document.getElementById('racialDex').value = 2;
-		document.getElementById('racialCon').value = 0;
-		document.getElementById('racialInt').value = 1;
-		document.getElementById('racialWis').value = 0;
-		document.getElementById('racialCha').value = 0;		
-	} else if (id == "elfWood"){
-		document.getElementById('racialStr').value = 0;
-		document.getElementById('racialDex').value = 2;
-		document.getElementById('racialCon').value = 0;
-		document.getElementById('racialInt').value = 0;
-		document.getElementById('racialWis').value = 1;
-		document.getElementById('racialCha').value = 0;		
-	} else if (id == "elfDrow"){
-		document.getElementById('racialStr').value = 0;
-		document.getElementById('racialDex').value = 2;
-		document.getElementById('racialCon').value = 0;
-		document.getElementById('racialInt').value = 0;
-		document.getElementById('racialWis').value = 0;
-		document.getElementById('racialCha').value = 1;		
-	} else if (id == "halflingLightfoot"){
-		document.getElementById('racialStr').value = 0;
-		document.getElementById('racialDex').value = 2;
-		document.getElementById('racialCon').value = 0;
-		document.getElementById('racialInt').value = 0;
-		document.getElementById('racialWis').value = 0;
-		document.getElementById('racialCha').value = 1;		
-	} else if (id == "halflingStout"){
-		document.getElementById('racialStr').value = 0;
-		document.getElementById('racialDex').value = 2;
-		document.getElementById('racialCon').value = 1;
-		document.getElementById('racialInt').value = 0;
-		document.getElementById('racialWis').value = 0;
-		document.getElementById('racialCha').value = 0;		
-	} else if (id == "dragonborn"){
-		document.getElementById('racialStr').value = 2;
-		document.getElementById('racialDex').value = 0;
-		document.getElementById('racialCon').value = 0;
-		document.getElementById('racialInt').value = 0;
-		document.getElementById('racialWis').value = 0;
-		document.getElementById('racialCha').value = 1;		
-	} else if (id == "gnomeForest"){
-		document.getElementById('racialStr').value = 0;
-		document.getElementById('racialDex').value = 1;
-		document.getElementById('racialCon').value = 0;
-		document.getElementById('racialInt').value = 2;
-		document.getElementById('racialWis').value = 0;
-		document.getElementById('racialCha').value = 0;		
-	} else if (id == "gnomeRock"){
-		document.getElementById('racialStr').value = 0;
-		document.getElementById('racialDex').value = 0;
-		document.getElementById('racialCon').value = 1;
-		document.getElementById('racialInt').value = 2;
-		document.getElementById('racialWis').value = 0;
-		document.getElementById('racialCha').value = 0;		
-	} else if (id == "halfElf"){
-		document.getElementById('racialStr').value = 2;
-		document.getElementById('racialDex').value = 0;
-		document.getElementById('racialCon').value = 0;
-		document.getElementById('racialInt').value = 0;
-		document.getElementById('racialWis').value = 0;
-		document.getElementById('racialCha').value = 1;		
-	} else if (id == "halfOrc"){
-		document.getElementById('racialStr').value = 2;
-		document.getElementById('racialDex').value = 0;
-		document.getElementById('racialCon').value = 1;
-		document.getElementById('racialInt').value = 0;
-		document.getElementById('racialWis').value = 0;
-		document.getElementById('racialCha').value = 0;		
-	} else if (id == "tiefling"){
-		document.getElementById('racialStr').value = 0;
-		document.getElementById('racialDex').value = 0;
-		document.getElementById('racialCon').value = 0;
-		document.getElementById('racialInt').value = 1;
-		document.getElementById('racialWis').value = 0;
-		document.getElementById('racialCha').value = 2;		
-	} else{
-		document.getElementById('racialStr').value = 0;
-		document.getElementById('racialDex').value = 0;
-		document.getElementById('racialCon').value = 0;
-		document.getElementById('racialInt').value = 0;
-		document.getElementById('racialWis').value = 0;
-		document.getElementById('racialCha').value = 0;	
-	}
-	console.log(textIn);
-	dropdown(textIn, 'raceButton');
+	document.getElementById('racialStr').value = dataRace[id].racialStr;
+	document.getElementById('racialDex').value = dataRace[id].racialDex;
+	document.getElementById('racialCon').value = dataRace[id].racialCon;
+	document.getElementById('racialInt').value = dataRace[id].racialInt;
+	document.getElementById('racialWis').value = dataRace[id].racialWis;
+	document.getElementById('racialCha').value = dataRace[id].racialCha;
+
+	setDropText(textIn, 'raceButton');
 	showRaceImage(id);
 	update();
+	console.log(dataRace[id].description)
+
 };
 
 
@@ -173,16 +75,15 @@ function selectedClass(id, textIn){
 	var primaryAbility
 	var savingThrows
 	var proficiencies
-	dropdown(textIn, 'classButton');
+	setDropText(textIn, 'classButton');
 };
 
 
 //This function sets the name of the dropdown to the selected token
-function dropdown(val, name) {
+function setDropText(val, name) {
 	var y = document.getElementsByName(name);
 	var aNode = y[0].innerHTML = val + ' <span class="caret"></span>'; // Append 
 };
-
 
 
 function update(){
@@ -268,52 +169,8 @@ function showRaceImage(selRace){
 	}
 	var img = document.createElement("IMG");
 
-	if(selRace == "human"){
-		img.setAttribute('src', 'http://cdn.obsidianportal.com/images/65157/keldar.jpg');
-		dv.appendChild(img);
-		console.log('Should be showing an image of a human')
-	} else if (selRace == "dwarfHill"){
-		img.setAttribute('src', 'http://vignette1.wikia.nocookie.net/shattered-lands/images/9/95/Ulfberht.jpg/revision/latest?cb=20130213153257');
-		dv.appendChild(img);
-	} else if (selRace == "dwarfMountain"){
-		img.setAttribute('src', 'http://vignette1.wikia.nocookie.net/shattered-lands/images/9/95/Ulfberht.jpg/revision/latest?cb=20130213153257');
-		dv.appendChild(img);
-	} else if (selRace == "elfHigh"){
-		img.setAttribute('src', 'http://varg.wdfiles.com/local--files/se-mage/19455High_Elf_Avatar_by_Graysun_D.jpg');
-		dv.appendChild(img);	
-	} else if (selRace == "elfWood"){
-		img.setAttribute('src', 'http://varg.wdfiles.com/local--files/se-mage/19455High_Elf_Avatar_by_Graysun_D.jpg');
-		dv.appendChild(img);	
-	} else if (selRace == "elfDrow"){
-		img.setAttribute('src', 'http://varg.wdfiles.com/local--files/se-mage/19455High_Elf_Avatar_by_Graysun_D.jpg');
-		dv.appendChild(img);	
-	} else if (selRace == "halflingLightfoot"){
-		img.setAttribute('src', 'https://db4sgowjqfwig.cloudfront.net/images/920420/Rosie_Cusswell.jpg');
-		dv.appendChild(img);	
-	} else if (selRace == "halflingStout"){
-		img.setAttribute('src', 'https://db4sgowjqfwig.cloudfront.net/images/920420/Rosie_Cusswell.jpg');
-		dv.appendChild(img);
-	} else if (selRace == "dragonborn"){	
-		img.setAttribute('src', 'https://s-media-cache-ak0.pinimg.com/564x/73/78/65/737865c6dbf5c9fdd4b7d982ae096b04.jpg');
-		dv.appendChild(img);
-	} else if (selRace == "gnomeForest"){
-		img.setAttribute('src', 'https://holiviantales.files.wordpress.com/2015/04/garl.png?w=470');
-		dv.appendChild(img);	
-	} else if (selRace == "gnomeRock"){
-		img.setAttribute('src', 'https://holiviantales.files.wordpress.com/2015/04/garl.png?w=470');
-		dv.appendChild(img);		
-	} else if (selRace == "halfElf"){
-		img.setAttribute('src', 'https://s-media-cache-ak0.pinimg.com/originals/02/01/f5/0201f5e43c30ed7fabc517c8254245c8.jpg');
-		dv.appendChild(img);	
-	} else if (selRace == "halfOrc"){
-		img.setAttribute('src', 'https://s-media-cache-ak0.pinimg.com/564x/98/2a/a5/982aa5c0b1a43606f472a9273c0c9bef.jpg');
-		dv.appendChild(img);	
-	} else if (selRace == "tiefling"){
-		img.setAttribute('src', 'https://s-media-cache-ak0.pinimg.com/236x/51/33/34/513334f69ce4f629a4024a07f6d49d97.jpg');
-		dv.appendChild(img);
-	}else {
-		img.setAttribute('src', 'https://thewhisperingway.files.wordpress.com/2013/07/386_ca_cleric2.jpg');
-		dv.appendChild(img);
-		console.log('Should be showing an image of a random guy')
-	}
+	img.setAttribute('src', dataRace[selRace].imgScr);
+	dv.appendChild(img);
 };
+
+
