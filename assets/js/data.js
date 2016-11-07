a652f3a96becb9a441c043aaeedb8e978f61056c
Raul Perez <zerepangel@gmail.com>
2016-11-07 18:31:03 +0100

    Added data.js where the variables will be stored

---
+++ assets/js/data.js
@@ -0,0 +1,140 @@
+
+
+var race = {
+	'human': {
+		'racialStr' : 1,
+		'racialDex' : 1,
+		'racialCon' : 1,
+		'racialInt' : 1,
+		'racialWis' : 1,
+		'racialCha' : 1,
+		'imgScr' : 1
+	},	
+	'dwarfHill': {
+		'racialStr' : 0,
+		'racialDex' : 0,
+		'racialCon' : 2,
+		'racialInt' : 0,
+		'racialWis' : 1,
+		'racialCha' : 0,
+		'imgScr' : 1
+	},	
+	'dwarfMountain': {
+		'racialStr' : 2,
+		'racialDex' : 0,
+		'racialCon' : 2,
+		'racialInt' : 0,
+		'racialWis' : 0,
+		'racialCha' : 0,
+		'imgScr' : 1
+	},	
+	'elfHigh': {
+		'racialStr' : 0,
+		'racialDex' : 2,
+		'racialCon' : 0,
+		'racialInt' : 1,
+		'racialWis' : 0,
+		'racialCha' : 0,
+		 'imgScr' : 1
+	},	
+	'elfWood': {
+		'racialStr' : 0,
+		'racialDex' : 2,
+		'racialCon' : 0,
+		'racialInt' : 0,
+		'racialWis' : 1,
+		'racialCha' : 0,
+		 'imgScr' : 1
+	},	
+	'elfDrow': {
+		'racialStr' : 0,
+		'racialDex' : 2,
+		'racialCon' : 0,
+		'racialInt' : 0,
+		'racialWis' : 0,
+		'racialCha' : 1,
+		 'imgScr' : 1
+	},	
+	'halflingLightfoot': {
+		'racialStr' : 0,
+		'racialDex' : 2,
+		'racialCon' : 0,
+		'racialInt' : 0,
+		'racialWis' : 0,
+		'racialCha' : 1,
+		'imgScr' : 0
+		
+	},	
+	'halflingStout': {
+		'racialStr' : 0,
+		'racialDex' : 2,
+		'racialCon' : 1,
+		'racialInt' : 0,
+		'racialWis' : 0,
+		'racialCha' : 0, 
+		'imgScr' : 0
+	},	
+	'dragonborn': {
+		'racialStr' : 2,
+		'racialDex' : 0,
+		'racialCon' : 0,
+		'racialInt' : 0,
+		'racialWis' : 0,
+		'racialCha' : 1, 
+		'imgScr' : 0
+	},	
+	'gnomeForest': {
+		'racialStr' : 0,
+		'racialDex' : 1,
+		'racialCon' : 0,
+		'racialInt' : 2,
+		'racialWis' : 0,
+		'racialCha' : 0, 
+		'imgScr' : 0
+	},	
+	'gnomeRock': {
+		'racialStr' : 0,
+		'racialDex' : 0,
+		'racialCon' : 1,
+		'racialInt' : 2,
+		'racialWis' : 0,
+		'racialCha' : 0, 
+		'imgScr' : 0
+	},	
+	'halfElf': {
+		'racialStr' : 2,
+		'racialDex' : 0,
+		'racialCon' : 0,
+		'racialInt' : 0,
+		'racialWis' : 0,
+		'racialCha' : 1, 
+		'imgScr' : 0
+	},	
+	'halfOrc': {
+		'racialStr' : 2,
+		'racialDex' : 0,
+		'racialCon' : 1,
+		'racialInt' : 0,
+		'racialWis' : 0,
+		'racialCha' : 0, 
+		'imgScr' : 0
+	},	
+	'tiefling': {
+		'racialStr' : 0,
+		'racialDex' : 0,
+		'racialCon' : 0,
+		'racialInt' : 1,
+		'racialWis' : 0,
+		'racialCha' : 2, 
+		'imgScr' : 0
+	},
+	'another': {
+		'racialStr' : 0,
+		'racialDex' : 0,
+		'racialCon' : 0,
+		'racialInt' : 0,
+		'racialWis' : 0,
+		'racialCha' : 0, 
+		'imgScr' : 0
+	}
+};
