a10b5f73ac9e8f10c9f16961f6bc179d5c978106
Raul Perez <zerepangel@gmail.com>
2016-11-15 13:49:04 +0100

    Adding a React branch

---
+++ app.js
@@ -0,0 +1,23 @@
+var express 	= require('express');
+var path 		= require('path');
+var app 		= express();
+var fs 			= require('fs');
+var port 		= process.env.PORT || 8080;
+var router 		= express.Router(); 
+
+app.use(express.static(path.join(__dirname + '/node_modules')));
+app.use(express.static(path.join(__dirname + '/assets')));
+
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+// REGISTER OUR router 
+app.use('/', router);
+
+// START THE SERVER
+// =============================================================================
+
+var server = app.listen(port, function(){
+	console.log('Server listening on port 8080');
+});
